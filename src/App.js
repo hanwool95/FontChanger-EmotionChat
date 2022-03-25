4401e6afa45e780acc28d5db55f532c743cda186
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-03-25 14:46:07 +0900

    Feat: Emotion value & Camera

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import TextField from '@material-ui/core/TextField';
 import io from 'socket.io-client'
+
+import { Camera } from "expo-camera";
 
 const socket = io.connect('ws://127.0.0.1:3737');
 
@@ -10,7 +12,7 @@ let api_keys = "api_key="+process.env.REACT_APP_KEY+"&api_secret="+process.env.R
 function App() {
 
     // useState를 활용하면 class 없이 상태와 set 선언 가능.
-    const [state, setState] = useState({message:'', name:'', emotion:''})
+    const [state, setState] = useState({message:'', name:''})
     const [chat,setChat] = useState([])
 
     useEffect(() =>{
@@ -26,7 +28,8 @@ function App() {
     }
     const onMessageSubmit =(e)=>{
         e.preventDefault()
-        let {name, message, emotion} = state
+        let {name, message} = state
+        let emotion
 
         fetch("/image_api/facepp/v3/detect", {
           headers: {
@@ -36,10 +39,8 @@ function App() {
             body: api_keys+"&image_url=https://avatars.githubusercontent.com/u/77750865?v=4" +
                 "&return_attributes=emotion"
         }).then((response) => response.json()).then((data) =>
-                        console.log(data.faces[0].attributes.emotion));
+            emotion=JSON.stringify(data.faces[0].attributes.emotion)).then(() => socket.emit('serverReceiver', {name, message, emotion}))
 
-        // 실험 끝나면 socket emit을 promise 안에 넣어야 할 듯.
-        socket.emit('serverReceiver', {name, message, emotion})
         //초기화
         setState({message : '', name})
     }
@@ -48,6 +49,7 @@ function App() {
         return chat.map(({name, message, emotion}, index)=>(
             <div key={index}>
                 <h3>{name}:<span>{message}</span></h3>
+                <h5>{emotion}</h5>
             </div>
         ))
     }
@@ -82,6 +84,36 @@ function App() {
     );
 }
 
+function MyCamera() {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [isCameraReady, setIsCameraReady] = useState(false);
+    const [cameraSource, setCameraSource] = useState(null);
+    const cameraRef = useRef();
 
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync()();
+            setHasPermission(status === "granted");
+            console.log("camera granted")
+        })();
+        }, []);
+
+    const onCameraReady = () => {
+        setIsCameraReady(true);
+    };
+
+    const takePicture = async () => {
+        console.log("takking picture")
+        const options = { quality: 0.5, base64: true, skipProcessing: true };
+        const data = await cameraRef.current.takePictureAsync(options);
+        const source = data.uri;
+        await cameraRef.current.pausePreview();
+        setCameraSource(source)
+        console.log("picture source", source);
+    };
+    return (
+        <Camera ref={cameraRef} />
+    );
+}
 
 export default App;
