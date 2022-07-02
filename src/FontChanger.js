b2ff8ad4a5469d37a03805ac7d1f78add5eedc99
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-07-02 18:18:03 +0900

    Feat: Get next font when neutral or disgust

--- src/FontChanger.js
+++ src/FontChanger.js
@@ -8,12 +8,11 @@ const FontChanger = {
     'surprise': "Surprice",
 
     stringToDict: function(stringDict){
-
         let dict = {}
         stringDict = stringDict.replace('{', "")
         let newStringDict = stringDict.replace('}', "")
         let stringArray = newStringDict.split(',');
-        stringArray.reduce((i, strData)=> {
+        stringArray.map((strData)=> {
             let leftRightArray = strData.split(':')
             let key = leftRightArray[0].replace('\"', "").replace('\"', "")
             dict[key] = parseFloat(leftRightArray[1])
@@ -33,7 +32,13 @@ const FontChanger = {
         sortable.sort((a,b) => {
             return b[1] - a[1];
         })
+        console.log(sortable)
         let font = this[sortable[0][0]]
+        let index = 1;
+        while (font === 'Nanum Gothic'){
+            font = this[sortable[index][0]]
+            index += 1;
+        }
         console.log(font)
         return font
     }
